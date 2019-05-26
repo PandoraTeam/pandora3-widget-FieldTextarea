82c142d3ce63726b98295c5e910b4b2ce04abf9e
Petr Gostin <U.238@rambler.ru>
2019-05-26 17:28:47 +0300

    initial commit

--- FieldTextarea.php
+++ FieldTextarea.php
@@ -1,1 +1,38 @@
+<?php
+namespace Pandora3\Widgets\FieldTextarea;
 
+use Pandora3\Widgets\FormField\FormField;
+
+/**
+ * Class FieldTextarea
+ * @package Pandora3\Widgets\FieldTextarea
+ */
+class FieldTextarea extends FormField {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function getView(): string {
+		return __DIR__.'/Views/Widget';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function beforeRender(array $context): array {
+		$attribs = $context['attribs'] ?? '';
+		if ($context['placeholder'] ?? '') {
+			$attribs .= ' placeholder="'.$context['placeholder'].'"';
+		}
+		$height = $context['height'] ?? '';
+		if ($height) {
+			if (is_numeric($height)) {
+				$height .= 'px';
+			}
+			$attribs .= ' style="height: '.$height.'"';
+		}
+		$context['attribs'] = $attribs;
+		return $context;
+	}
+
+}
